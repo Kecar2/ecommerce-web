709e7b7c38e14dbd7252c25c99d8fd51eeb52bd7
Kecar2 <kendrycarvajalsegura@gmail.com>
2022-08-08 10:27:12 +0200

    funcionalidades botones añadir a la bolsa y +- cantidad de productos

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,13 +2,21 @@ import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 
 import { client, urlFor } from '../../lib/client';
-import { Product } from '../../components'
+import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
 
     const { image, name, details, price } = product;
 
     const [index, setIndex] = useState(0);
+    const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+
+    const handleBuyNow = () => {
+        onAdd(product, qty);
+    
+        setShowCart(true);
+      }
 
     return (
         <div>
@@ -49,14 +57,14 @@ const ProductDetails = ({ product, products }) => {
                     <div className="quantity">
                         <h3>Cantidad:</h3>
                         <p className="quantity-desc">
-                            <span className="minus"><AiOutlineMinus /></span>
-                            <span className="num">0</span>
-                            <span className="plus"><AiOutlinePlus /></span>
+                            <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
+                            <span className="num" onClick="">{qty}</span>
+                            <span className="plus" onClick={incQty}><AiOutlinePlus /></span>
                         </p>
                     </div>
                     <div className="buttons">
-                        <button type="button" className="add-to-cart">Añadir a la Bolsa</button>
-                        <button type="button" className="buy-now">Comprar Ahora</button>
+                        <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Añadir a la Bolsa</button>
+                        <button type="button" className="buy-now" onClick={handleBuyNow}>Comprar Ahora</button>
                     </div>
                 </div>
             </div>
